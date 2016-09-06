453556438eb97f95d58e1b552e96b104e93ca885
arvinddhandapani <arvind.mib@gmail.com>
2016-09-07 00:17:30 +0530

    First Sync
    Adding first sync

---
+++ seachange_server.sql
@@ -0,0 +1,45 @@
+CREATE DATABASE seachange_server;
+
+
+USE seachange_server;
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `angularcode_products`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE IF NOT EXISTS `server_master` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `sku` int(50) NOT NULL,
+  `server_name` varchar(100) NOT NULL,
+  `server_ip` double NOT NULL,
+  `server_username` varchar(100) NOT NULL,
+  `server_password` varchar(100) NOT NULL,
+  `description` varchar(500) NOT NULL,
+  `category` int(11) NOT NULL,
+  `status` varchar(10) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=951 ;
+
+--
+-- Dumping data for table `products`
+--
+
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
